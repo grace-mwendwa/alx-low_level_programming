674b77cf7968c99e994b0818a8ad73ae7ad15415
Grace.Mwendwa <kivuti.grace@gmail.com>
2023-04-20 17:41:03 -0700

    correct8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Write a C program that prints and that piece of art is useful\"
- * Return: 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
